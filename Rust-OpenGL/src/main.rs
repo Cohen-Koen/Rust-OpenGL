0881d0ce82874101e24e5e501e144a61b8b96fff
Cohen-Koen <deathbydickey@gmail.com>
2022-07-08 18:07:59 +0000

    cleared up some stuff with comments

--- Rust-OpenGL/src/main.rs
+++ Rust-OpenGL/src/main.rs
@@ -5,7 +5,8 @@
 const WINDOW_TITLE: &str = "Hello Window";
 
 use beryllium::*;
-
+use ogl33::load_gl_with;
+use ogl33::glClearColor;
 fn main() {
   let sdl = SDL::init(InitFlags::Everything).expect("couldn't start SDL");
   sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
@@ -17,7 +18,7 @@ fn main() {
       .unwrap();
   }
 
-  let _win = sdl
+  let win = sdl         // This is where we declare our window as a thing
     .create_gl_window(
       WINDOW_TITLE,
       WindowPosition::Centered,
@@ -27,7 +28,7 @@ fn main() {
     )
     .expect("couldn't make a window and context");
 
-  'main_loop: loop {
+  'main_loop: loop { 
     // handle events this frame
     while let Some(event) = sdl.poll_events().and_then(Result::ok) {
       match event {
@@ -38,5 +39,9 @@ fn main() {
     // now the events are clear.
 
     // here's where we could change the world state and draw.
+    unsafe {load_gl_with(|f_name| win.get_proc_address(f_name));} //load openGL
+    unsafe {glClearColor(0.2, 0.3, 0.3, 1.0);} //clear color
+
+
   }
 }
